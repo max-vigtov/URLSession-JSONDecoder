6b9cc57afb7954612b5cc2c2b973202010553108
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-07-19 18:16:35 -0600

    Initial Commit

---
+++ URLSessionJSONDecoder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLSessionJSONDecoder
+//
+//  Created by Max Vigueras Tovar on 19/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLSessionJSONDecoder/URLSessionJSONDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionJSONDecoderApp.swift
+//  URLSessionJSONDecoder
+//
+//  Created by Max Vigueras Tovar on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionJSONDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
